5b77fb1ad5d0f9eea753655bdc7ddfd605087eb5
meviktor <mviktor9000@gmail.com>
2021-01-16 22:52:10 +0100

    Fixes in the config for building production code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 module.exports = {
     entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
     module: {
         rules: [
           {
             test: /\.(js)$/,
             exclude: /node_modules/,
             use: ['babel-loader']
+          },
+          {
+            test: /\.(css)$/,
+            use: ['style-loader', 'css-loader'],
           }
         ]
     },
     resolve: {
         extensions: ['*', '.js']
-    },
-    output: {
-      path: '/dist',
-      filename: 'bundle.js'
     },
     plugins: [
       new HtmlWebpackPlugin({
